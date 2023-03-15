9c818a1adaa970d46b00eca730f8dd9986bd3c36
dnash <dnash@skyriver.ucsd.edu>
2023-03-15 16:30:25 -0700

    rerun all figs

--- preprocess/png_to_pdf.py
+++ preprocess/png_to_pdf.py
@@ -1,7 +1,7 @@
 # Standard Python modules
 from PIL import Image
 
-for i in range(6):
+for i in range(1, 7):
     infname = '../figs/final_figs/fig{0}.png'.format(str(i))
     outfname = '../figs/final_figs_pdf/fig{0}.pdf'.format(str(i))
 
