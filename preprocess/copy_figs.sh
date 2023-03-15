e3f67a8a522ca5c146fe985e16e34ddccc0ae7e4
dnash <dnash@skyriver.ucsd.edu>
2023-03-15 13:23:30 -0700

    update windrose plot and bar chart clean up directory organization

---
+++ preprocess/copy_figs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+######################################################################
+# Filename:    copy_figs.sh
+# Author:      Deanna Nash dlnash@ucsb.edu
+# Description: Script to copy final figures to one folder
+#
+######################################################################
+
+# Input parameters
+maindir="../figs/" # main figure folder
+finaldir="../figs/final_figs/" # final figure folder
+
+# fig names in main folder
+array=(
+elevation_7.5arcsec_with_inset
+ar_barplot
+extreme-AR_IVT-250Z_composite_final
+extreme-AR_windrose_elev-overlay_daily
+extreme-AR_prec_composite_all_daily
+percentile_bins_daily
+)
+
+# new names to be fig<name given in array2>
+array2=(
+1
+2
+3
+4
+5
+6
+)
+
+
+
+for i in ${!array[*]}
+do 
+    infile="${maindir}${array[$i]}.png"
+    outfile="${finaldir}fig${array2[$i]}.png"
+#     echo "${infile} to ${outfile}"
+    cp -v ${infile} ${outfile}
+done
+
+# ### supplemental figs
+# supp_array=(
+# composite_ar_types_bias
+# )
+
+# ## new names to be given
+# supp_array2=(
+# 1
+# )
+# for i in ${!supp_array[*]}
+# do 
+#     infile="${maindir}${supp_array[$i]}.png"
+#     outfile="${finaldir}figS${supp_array2[$i]}.png"
+# #     echo "${infile} to ${outfile}"
+#     cp -v ${infile} ${outfile}
+# done
